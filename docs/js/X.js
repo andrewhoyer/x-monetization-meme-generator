2628c643bf677b27ed4281e65bd5aa4313846041
admin <andrewhoyer@gmail.com>
2023-10-14 19:41:36 -0700

    Code clean-up

--- docs/js/X.js
+++ docs/js/X.js
@@ -1,11 +1,8 @@
 $(document).ready(function() {
 	
-	console.log(location.search); // Assume location.search = "?a=1&b=2b2"
-	
-	let params = new URLSearchParams(location.search.substring(1));
-	
+	let params   = new URLSearchParams(location.search.substring(1));
 	let username = params.get('username');
-	let amount = params.get('amount');
+	let amount   = params.get('amount');
 	
 	$('#username').html(username);
 	$('#amount').html(amount);
